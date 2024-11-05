3c30dd916d3e4b4d4e9a926d4792a2d9616575ec
XOneFG <117354752+VsrStudio@users.noreply.github.com>
2024-11-05 09:26:57 +0700

    Update BlockParty.php

--- src/BlockParty/BlockParty.php
+++ src/BlockParty/BlockParty.php
@@ -3,29 +3,12 @@
 namespace BlockParty;
 
 use pocketmine\plugin\PluginBase;
-use pocketmine\command\Command;
-use pocketmine\command\CommandSender;
 
 class BlockParty extends PluginBase {
 
-    private $gameManager;
-    private $statsManager;
-    private $commandHandler;
-
     public function onEnable(): void {
-        $this->saveDefaultConfig(); // Simpan konfigurasi jika belum ada
         $this->getServer()->getCommandMap()->register("blockparty", new CommandHandler($this));
-        $this->getServer()->getPluginManager()->registerEvents(new EventListener($this), $this);
-        $this->gameManager = new GameManager($this);
-        $this->statsManager = new StatsManager($this);
-        $this->commandHandler = new CommandHandler($this);
+        // Kode lainnya...
     }
-
-    public function getGameManager(): GameManager {
-        return $this->gameManager;
-    }
-
-    public function getStatsManager(): StatsManager {
-        return $this->statsManager;
-    }
+    // Kode lainnya...
 }
